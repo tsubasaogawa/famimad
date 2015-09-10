7f272627a7db748099d424adb69dc0c9bab166a4
Tsubasa Ogawa <hurikake@gmail.com>
2015-09-11 00:41:05 +0900

    fix typo & add comments

--- famimad.rb
+++ famimad.rb
@@ -2,40 +2,69 @@
 require 'rubygems'
 require 'daemons'
 
-# FAMIMA program
+#= FAMIMA program
+#Authors:: Tsubasa Ogawa
+#Version:: 1.0
 
+# ------------- Settings ------------- # 
+
+# Path to Aquestalk binary
 AQUESTALK_BIN = '/usr/local/bin/AquesTalkPi'
-PLAYER_BIN = 'aplay'
-WATCHING_FILE = '/tmp/famimad.log'
-FAMIMA_FILE = './famima.wav'
-WAIT_SEC = 1 # sec
 
+# Path to a sound player
+PLAYER_BIN = 'aplay'
+
+# Path for a log file
+FAMIMAD_LOG = '/tmp/famimad_log'
+
+# Path to the log file which MOTION outputs
+WATCHING_FILE = '/tmp/famima.log'
+
+# Path to famima sound
+FAMIMA_FILE = '/home/pi/dev/famima/famima.wav'
+
+# A loop interval (sec.)
+WAIT_SEC = 1
+
+# ------------------------------------ #
+
+# AquesTalk
 class AquesTalk
   def initialize
   end
 
   def talk(text)
     system("#{AQUESTALK_BIN} '#{text}' | #{PLAYER_BIN}")
+    debug("Aquestalk said: #{text}")
   end
 end
 
+# Watching the log file outputted by motion
 class MotionWatch
   def initialize
     @modified_time = nil
   end
 
   def triggered?
-    file = File::stat(WATCHCING_FILE)
+    return false if ! File.exist?(WATCHING_FILE)
+
+    # get timestamps of the log file
+    file = File::stat(WATCHING_FILE)
     new_modified_time = file.mtime.to_s
+
+    # modified (equals: motion detected)
     if @modified_time != new_modified_time
       @modified_time = new_modified_time
+      debug("motion triggered at #{new_modified_time}")
       true
     else
+      debug("not triggered")
       false
     end
   end
 end
 
+# playing famima sound
 class FamimaSound
   def initialize
   end
@@ -46,24 +75,28 @@ class FamimaSound
   end
 end
 
+# for debugging
 def debug(text)
   # STDERR.puts(text)
+  system("echo #{text} >> #{FAMIMAD_LOG}")
 end
 
-# initialize
-_, term, = ARGV
-term = term.to_i
-
-aquestalk = AquesTalk.new
-aquestalk.talk("kidou kanryou!")
-
-mw = MotionWatch.new
-famima = FamimaSound.new
-
+# main proc.
 Daemons.run_proc(File.basename(__FILE__)) do
-  while true
+
+  # initialize
+  aquestalk = AquesTalk.new
+  aquestalk.talk("kidou kanryou!")
+
+  mw = MotionWatch.new
+  famima = FamimaSound.new
+
+  # infinite loop
+  loop do
     famima.play if mw.triggered?
     sleep WAIT_SEC
   end
+
+  debug("loop end")
 end
 
