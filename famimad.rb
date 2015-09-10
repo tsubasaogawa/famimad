f94ef55f68273eeef3bf0bdddd5d1406171fa786
Tsubasa Ogawa <hurikake@gmail.com>
2015-09-10 23:47:52 +0900

    change from watching jpeg files to a log file motiond outputs

--- famimad.rb
+++ famimad.rb
@@ -4,11 +4,10 @@ require 'daemons'
 
 # FAMIMA program
 
-CAPTURED_PATH = '/tmp/motion'
-CAPTURED_FILEMASK = '*.jpg'
 AQUESTALK_BIN = '/usr/local/bin/AquesTalkPi'
 PLAYER_BIN = 'aplay'
-FAMIMA_FILE = '~/dev/famima/famima.wav'
+WATCHING_FILE = '/tmp/famimad.log'
+FAMIMA_FILE = './famima.wav'
 WAIT_SEC = 1 # sec
 
 class AquesTalk
@@ -20,33 +19,20 @@ class AquesTalk
   end
 end
 
-class PhotoWatch
-  def initialize(path)
-    # Dir.chdir(path)
-    @path = path
-    @photo_count = filecount
-    debug("initial file count = #{@photo_count}")
+class MotionWatch
+  def initialize
+    @modified_time = nil
   end
 
-  def increment?
-    count = filecount
-    debug("now file count = #{count}")
-    if @photo_count + 1 == count
-      @photo_count = count
+  def triggered?
+    file = File::stat(WATCHCING_FILE)
+    new_modified_time = file.mtime.to_s
+    if @modified_time != new_modified_time
+      @modified_time = new_modified_time
       true
     else
-      @photo_count = count if @photo_count < count # more than +1
       false
     end
-  end
-
-  def watch(famima)
-    famima.play if increment?
-  end
-
-  private
-  def filecount
-    Dir.glob("#{@path}/#{CAPTURED_FILEMASK}").count
   end
 end
 
@@ -71,13 +57,12 @@ term = term.to_i
 aquestalk = AquesTalk.new
 aquestalk.talk("kidou kanryou!")
 
-pw = PhotoWatch.new(CAPTURED_PATH)
-
+mw = MotionWatch.new
 famima = FamimaSound.new
 
 Daemons.run_proc(File.basename(__FILE__)) do
   while true
-    pw.watch(famima)
+    famima.play if mw.triggered?
     sleep WAIT_SEC
   end
 end
